a2365b7fee7fa79eeaf39f866e85632026697bf4
kjdowns <kevin.downs04@gmail.com>
2021-05-13 15:35:33 -0400

    add goals-list route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GoalsListComponent } from './components/goals/goals-list/goals-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Goals routes
+  {path: '', component: GoalsListComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
