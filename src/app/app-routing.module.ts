bb2188fed9eb89b13ee4e6786749f35fbcb8b688
Keila Cressman <kcressman@xpanxion.com>
2021-05-13 16:21:10 -0400

    updated goals-add path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GoalsAddComponent } from './components/goals/goals-add/goals-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'goals/add', component: GoalsAddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
