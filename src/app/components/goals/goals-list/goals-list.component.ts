ff8290653e09280f4627b3800f0f4775a07d4544
kjdowns <kevin.downs04@gmail.com>
2021-05-13 16:50:55 -0400

    add stub of goals list component

--- src/app/components/goals/goals-list/goals-list.component.ts
+++ src/app/components/goals/goals-list/goals-list.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsListComponent implements OnInit {
 
+  GOALS = [
+    {
+      id: 1,
+      name: "New Car",
+      description: "Saving for a brand new car",
+      picture: null,
+      targetDate: '2022-01-01',
+      currentAmount: 1000.00,
+      targetAmount: 23000.00
+    },
+    {
+      id: 2,
+      name: "PlayStation 5",
+      description: "Saving for a PlayStation 5",
+      picture: null,
+      targetDate: '2021-10-01',
+      currentAmount: 500.00,
+      targetAmount: 600.00
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  private displayProgress(current, total){
+    return Number(current / total) * 100 ;
   }
 
 }
