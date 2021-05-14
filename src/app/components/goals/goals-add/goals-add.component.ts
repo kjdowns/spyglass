2af42ab2673f81ef6826ca1e9136c27f102e19fe
Keila Cressman <kcressman@xpanxion.com>
2021-05-14 10:18:10 -0400

    goalsadd update

--- src/app/components/goals/goals-add/goals-add.component.ts
+++ src/app/components/goals/goals-add/goals-add.component.ts
@@ -1,15 +1,112 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+interface Picture {
+  value: string;
+  image?: string; 
+}
 
 @Component({
   selector: 'app-goals-add',
   templateUrl: './goals-add.component.html',
   styleUrls: ['./goals-add.component.css']
 })
+
 export class GoalsAddComponent implements OnInit {
+
+  pictures: Picture[]= [
+    {value:"House", image: "../assets/images/house.png"},
+    {value:"Car", image: "../assets/images/car.png"},
+    {value:"Wedding", image: "../assets/images/wedding.png"}
+  ];
+
+  goals = {
+    name: '',
+    description: '',
+    picture: null,
+    targetDate: '',
+    currentAmount: null,
+    targetAmount: null
+  };
+
+  submitted = false;
+
+  name = new FormControl('', [Validators.required]);
+  description = new FormControl('', [Validators.required]);
+  targetDate = new FormControl('', [Validators.required]);
+  currentAmount = new FormControl('', [Validators.required]);
+  targetAmount = new FormControl('', [Validators.required]);
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  saveGoal(): void {
+    const data = {
+      name: this.goals.name,
+      description: this.goals.description,
+      picture: this.goals.picture,
+      targetDate: this.goals.targetDate,
+      currentAmount: this.goals.currentAmount,
+      targetAmount: this.goals.currentAmount
+    };
+
+    // this.goalService.create(data)
+    //   .subscribe(
+    //     response => {
+    //       console.log(response);
+    //       this.submitted = true;
+    //     },
+    //     error => {
+    //       console.log(error);
+    //     });
+  }
+
+  newGoal(): void {
+    this.submitted = false;
+    this.goals = {
+      name: '',
+      description: '',
+      picture: null,
+      targetDate: '',
+      currentAmount: null,
+      targetAmount: null
+    }
+  }
+
+  getErrorName() {
+    if (this.name.hasError('required')) {
+      return 'Name missing';
+    }
+    return this.name.hasError('name') ? 'Not a valid name' : '';
+  }
+
+  getErrorDescription() {
+    if (this.description.hasError('required')) {
+      return 'Description missing';
+    }
+    return this.description.hasError('description') ? 'Not a valid description' : '';
+  }
+
+  getErrorTargetDate() {
+    if (this.targetDate.hasError('required')) {
+      return 'Target Date missing';
+    }
+    return this.targetDate.hasError('targetDate') ? 'Not a valid targetDate' : '';
+  }
+
+  getErrorCurrentAmount() {
+    if (this.currentAmount.hasError('required')) {
+      return 'Current Amount missing';
+    }
+    return this.currentAmount.hasError('currentAmount') ? 'Not a valid currentAmount' : '';
+  }
+
+  getErrorTargetAmount() {
+    if (this.targetAmount.hasError('required')) {
+      return 'Target Amount missing';
+    }
+    return this.targetAmount.hasError('targetAmount') ? 'Not a valid targetAmount' : '';
+  }
 }
