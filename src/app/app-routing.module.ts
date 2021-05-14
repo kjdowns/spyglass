4d5e58945e4031f01f2d5135616de03df9b765d6
kjdowns <kevin.downs04@gmail.com>
2021-05-14 12:27:36 -0400

    add initial detail component setup including routes and links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GoalsAddComponent } from './components/goals/goals-add/goals-add.component';
+import { GoalsDetailsComponent } from './components/goals/goals-details/goals-details.component';
 import { GoalsListComponent } from './components/goals/goals-list/goals-list.component';
 
 const routes: Routes = [
   // Goals routes
   {path: '', component: GoalsListComponent},
-  {path: 'goals/add', component: GoalsAddComponent}
+  {path: 'goals/add', component: GoalsAddComponent},
+  {path: 'goals/:id', component: GoalsDetailsComponent}
   
 ];
 
