f746a906c587408117290531d790f67a16fcbd6c
kjdowns <kevin.downs04@gmail.com>
2021-05-14 10:37:43 -0400

    add goal model

---
+++ src/app/models/goal.ts
@@ -0,0 +1,10 @@
+
+export class Goal {
+    id?: number;
+    name: string;
+    description: string;
+    picture: string;
+    targetDate: Date;
+    targetAmount: number;
+    currentAmount: number
+}
