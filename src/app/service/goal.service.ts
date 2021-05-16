8a6f947e1300fd13304f56d410d42636f99a555d
kjdowns <kevin.downs04@gmail.com>
2021-05-16 13:26:20 -0400

    update service to utilize postgres database and return observables

--- src/app/service/goal.service.ts
+++ src/app/service/goal.service.ts
@@ -3,61 +3,39 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Goal } from '../models/goal';
 
-let baseUrl = '';
+let baseUrl = 'http://localhost:8080/goals';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoalService {
 
-  GOALS: Goal[] = [
-    {
-      id: 1,
-      name: "New Car",
-      description: "Saving for a brand new car",
-      picture: null,
-      targetDate: new Date('2022-01-01'),
-      currentAmount: 1000.00,
-      targetAmount: 23000.00
-    },
-    {
-      id: 2,
-      name: "PlayStation 5",
-      description: "Saving for a PlayStation 5",
-      picture: null,
-      targetDate: new Date('2021-10-01'),
-      currentAmount: 500.00,
-      targetAmount: 600.00
-    }
-  ];
-
   constructor(private http: HttpClient) { }
 
-  getAll(): Goal[] {
-    return this.GOALS;
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
   }
 
-  get(id): Goal {
-    let selectGoal = this.GOALS.find(goal => id == goal.id);
-    return selectGoal;
+  get(id): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
   }
 
+  // should be removed once refactored - use getAll()
   getAll2(): Observable<any> {
     return this.http.get(baseUrl);
   }
   
+  // should be removed once refactored - use get(id)
   get2(orderId): Observable<any> {
     return this.http.get(`${baseUrl}/${orderId}`);
   }
   
   create(data): Observable<any> {
-    let baseUrl = 'http://localhost:8081/goals/add';
-    return this.http.post(baseUrl, data);
+    return this.http.post(`${baseUrl}/add`, data);
   }
   
   update(id, data): Observable<any> {
-    let baseUrl = 'http://localhost:8081/goals/update';
-    return this.http.put(`${baseUrl}/${id}`, data);
+    return this.http.put(`${baseUrl}/update/${id}`, data);
   }
   
   delete(id): Observable<any> {
