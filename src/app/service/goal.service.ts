24bf38ad1a407efa1508bc3e0196375ba18f5acd
kjdowns <kevin.downs04@gmail.com>
2021-05-16 13:26:51 -0400

    remove unused imports

--- src/app/service/goal.service.ts
+++ src/app/service/goal.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Goal } from '../models/goal';
 
 let baseUrl = 'http://localhost:8080/goals';
 
