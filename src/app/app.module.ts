cfaeb841ac819f854f259ef2852011632f35bd7d
kjdowns <kevin.downs04@gmail.com>
2021-05-17 11:30:49 -0400

    resolve merge issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,9 @@ import { MaterialModule } from './shared/material.module';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { GreetingComponent } from './components/greeting/greeting.component';
-=======
 import { ChartsProgressComponent } from './components/charts/charts-progress.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-
->>>>>>> 588c84a6b4e3ba88785b15d2828d629f6296db9a
 
 @NgModule({
   declarations: [
@@ -27,11 +23,8 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
     GoalsDetailsComponent,
     GoalsAddComponent,
     GoalsUpdateComponent,
-<<<<<<< HEAD
-    GreetingComponent
-=======
+    GreetingComponent,
     ChartsProgressComponent
->>>>>>> 588c84a6b4e3ba88785b15d2828d629f6296db9a
   ],
   imports: [
     BrowserModule,
