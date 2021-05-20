185fd1b9b0e90dbf37a6f7a7f6d6b7ad8c80ccb9
lorik <lorik10560@gmail.com>
2021-05-19 22:12:45 -0400

    JSON data hack for charts

--- src/app/components/charts/charts-progress.component.ts
+++ src/app/components/charts/charts-progress.component.ts
@@ -14,41 +14,32 @@ export class ChartsProgressComponent implements OnInit {
 
   goals: Goal [];
   private subscription; 
-  goalsChartArray = [{
-    "name": "TV",
-    "value": 1000
-   },{
-    "name": "Vacation",
-    "value": 5000
-   },{
-    "name": "Event",
-    "value": 83
-   }]
+  goalsChartArray = [];
   
       
-  goalsChart =
-   {
-    "name": "TV",
-    "value": 1000
-   };
-
+  goalsChart ={name: '', value:0}
+   
   constructor(private chartService: ChartService) { }
 
   ngOnInit(): void {
     this.subscription = this.chartService.getGoals().subscribe((data: any[])=> {
       this.goals = data;
+      // console.log('data from db', JSON.parse(JSON.stringify(data)));
+      
        this.getChartData();
+       this.goalsChartArray = [...this.goalsChartArray];
     })
   }
   
 getChartData(){
   this.goals.forEach((goal)=> {
-    for ( let i = 0; i < this.goalsChartArray.length; i++){
-      this.goalsChart[i].name = goal.name;
-     this.goalsChart[i].value = goal.currentAmount;
-    }
-     
-    console.log(this.goalsChart);
+    console.log(goal.name)
+      this.goalsChart.name = goal.name;
+     this.goalsChart.value = goal.currentAmount;
+         
+    this.goalsChartArray.push(this.goalsChart);
+    this.goalsChart = {name: '', value:0}
+     console.log(this.goalsChartArray)
     
   })
   }
